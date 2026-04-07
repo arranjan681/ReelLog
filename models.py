6cf133dae1779cb8f1d80965144840572102c367
arranjan681 <arranjan681@gmail.com>
2026-04-07 17:06:28 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,31 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_name = db.Column(db.String(150), nullable=False)
+    movie_name = db.Column(db.String(200), nullable=False)
     genre = db.Column(db.String(100), nullable=False)
-    release_year = db.Column(db.Integer, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
+    release_year = db.Column(db.String(10), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
     review = db.Column(db.Text, nullable=True)
-    poster_url = db.Column(db.String(500), nullable=True)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 class Watchlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_name = db.Column(db.String(150), nullable=False)
+    movie_name = db.Column(db.String(200), nullable=False)
     genre = db.Column(db.String(100), nullable=False)
-    release_year = db.Column(db.Integer, nullable=False)
-    added_at = db.Column(db.DateTime, nullable=False)
+    poster_url = db.Column(db.String(500), nullable=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
